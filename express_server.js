0997a1b6efab634abe8b81d816835a0e39e97292
ermurray <ermurray31@gmail.com>
2021-02-15 23:55:03 +0000

    added req handler for new url form input and blank definintion for random string generatior

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
@@ -6,12 +7,25 @@ const urlDatabase = {
   "b2xVn2": 'http://www.lighthouselabs.ca',
   "9sm5xK": 'http://www.google.com'
 };
+const generateRandomString = function() {
 
+};
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
