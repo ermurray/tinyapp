f0eaa999efa119fce9e4a517d490b21d7b1d4255
ermurray <ermurray31@gmail.com>
2021-02-15 21:43:16 +0000

    added html template to urls_index.ejs for the ejs view template

--- express_server.js
+++ express_server.js
@@ -10,7 +10,8 @@ const urlDatabase = {
 app.set('view engine', 'ejs');
 
 app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+  const templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
 });
 
 app.get('/hello', (req, res) => {
