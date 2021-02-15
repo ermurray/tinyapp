4169fd1ad7258c99e5a47a86b2aa692160053ae3
ermurray <ermurray31@gmail.com>
2021-02-15 22:56:45 +0000

    added the short url show request handler and used the req.params to add the short url and corresponding longurl to the view template

--- express_server.js
+++ express_server.js
@@ -9,9 +9,17 @@ const urlDatabase = {
 
 app.set('view engine', 'ejs');
 
-app.get('/urls.json', (req, res) => {
+app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render('urls_show', templateVars);
 });
 
 app.get('/hello', (req, res) => {
